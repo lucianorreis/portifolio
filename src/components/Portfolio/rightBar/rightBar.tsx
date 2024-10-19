fab79b3b5ed45a9d6bd54a67406e88583d2e17ce
Luciano.Reis <lrr.arms233@gmail.com>
2024-10-19 15:16:46 -0300

    Adicionando imagens no portfolio

--- src/components/Portfolio/rightBar/rightBar.tsx
+++ src/components/Portfolio/rightBar/rightBar.tsx
@@ -1,11 +1,26 @@
-import "./rightBar.scss"
+import "./rightBar.scss";
+import linkedinImg from "../../../assets/LinkedIn.png";
+import githubImg from "../../../assets/github.png";
 
-export function RightBar(){
-    return(
-        <div className="rightBar">
-        <section>
-
-        </section>
+export function RightBar() {
+  return (
+    <div className="rightBar">
+      <section>
+        <p>
+          <ul>
+            <li>
+              <a id="linkedin" href="/">
+                <img src={linkedinImg} alt="Linkedin Logo" width="70px" height="50px" />
+              </a>
+            </li>
+            <li>
+              <a id="github" href="/">
+                <img src={githubImg} alt="Github Logo" width="70px" height="50px" />
+              </a>
+            </li>
+          </ul>
+        </p>
+      </section>
     </div>
-    )
+  );
 }
